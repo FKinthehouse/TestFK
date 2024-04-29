95e14a89a9590623dd01ebfd1ecad5d07b028f18
Tariq Khan <tariqkhan@Tariqs-MacBook-Air.local>
2024-04-29 23:58:08 +0530

    first test made

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,5 +1,11 @@
 require 'StringCalculator'
 
 describe StringCalculator do
-	
+	describe ".add" do
+		context "given an empty string" do
+			it "returns zero" do
+				expect(StringCalculator.add("")).to eq(0)
+			end
+		end
+	end
 end
