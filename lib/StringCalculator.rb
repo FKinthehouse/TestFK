c3f2f00670d59c926389c3eddbab267d9e6c63ff
Tariq Khan <tariqkhan@Tariqs-MacBook-Air.local>
2024-04-30 12:12:03 +0530

    giving the string of numbers with comma

--- lib/StringCalculator.rb
+++ lib/StringCalculator.rb
@@ -1,7 +1,7 @@
 class StringCalculator
-	def self.add input
-	if input
-		return input.to_i
+	def self.add input1
+	if input1
+		return input1.split(',').inject(0) {|sum,i| sum+i.to_i}
 	else
 		return 0
  	end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -14,7 +14,7 @@ describe StringCalculator do
 	describe ".add" do
 		context "given a number 4" do
 			it "return 4" do
-			expect(StringCalculator.add(4)).to eq(4)
+			expect(StringCalculator.add("6,5")).to eq(11)
 		    end
 		end
 	end
