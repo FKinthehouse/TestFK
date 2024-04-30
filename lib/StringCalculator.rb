e1a27986f7aeb083f69348e94283d44dc1ce5b5e
Tariq Khan <tariqkhan@Tariqs-MacBook-Air.local>
2024-04-30 20:22:06 +0530

    refactored code and all 9test

--- lib/StringCalculator.rb
+++ lib/StringCalculator.rb
@@ -3,20 +3,14 @@ class StringCalculator
   def add string_of_nos
   	#checking string
 	  	raise "only string of number(s) required!!!" unless string_of_nos.is_a?(String)
-	    int_array=string_of_nos.split(/[^0-9-]+/).map(&:to_i) #converting into integer array
-	    int_array=int_array.select {|x| x<1000}
+	    int_array=string_of_nos.split(/[^0-9-]+/).map(&:to_i).select {|x| x<1000}   #converting into integer array
 	    raise "negative numbers not allowed <#{check_negatives(int_array)}>" if check_negatives(int_array)	
 	    int_array.reduce(0){|sum,i| sum+i} #for summation
   end
 
-# in the previous commit, I only added the definition, now implementing logic as it fails 
     def check_negatives num
   	   i=0
        num.each{|x| i+=1 if(x<0)}
-       if i==0
-       	return false
-       else
-       	return num.select{|x| x<0}.join(',')
-        end
+       i==0 ? (return false) : (return num.select{|x| x<0}.join(','))  
    end
 end
