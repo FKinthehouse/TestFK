d090714bb3afd00f5ac712d159bbdf0fef4ef827
Tariq Khan <tariqkhan@Tariqs-MacBook-Air.local>
2024-04-30 17:42:52 +0530

    test

--- lib/StringCalculator.rb
+++ lib/StringCalculator.rb
@@ -1,9 +1,23 @@
 class StringCalculator
-	def self.add input1
-	if input1
-		return input1.split(',').inject(0) {|sum,i| sum+i.to_i}
-	else
-		return 0
- 	end
-end
+# 	def self.add input1
+# 	if input1
+# 		return input1.split(',').inject(0) {|sum,i| sum+i.to_i}
+# 	else
+# 		return 0
+#  	end
+# end
+
+ def initialize
+ end
+
+
+  def add string_of_nos
+  	#checking string
+  	raise "only string of number(s) required!!!" unless string_of_nos.is_a?(String)
+    int_array=string_of_nos.split(/[^0-9-]+/).map(&:to_i) #converting into integer array
+    # raise "negative numbers not allowed <#{check_negatives(int_array)}>" if check_negatives(int_array)
+    int_array.reduce(0){|sum,i| sum+i} #for summation
+  end
+
+
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,21 +1,10 @@
 require 'StringCalculator'
 
-describe StringCalculator do
-	# describe ".add" do
-	# 	context "given an empty string" do
-	# 		it "returns zero" do
-	# 			expect(StringCalculator.add("")).to eq(0)
-	# 		end
-	# 	end
-	# end
 
-
-
-	describe ".add" do
-		context "given a number 4" do
-			it "return 4" do
-			expect(StringCalculator.add("6,5")).to eq(11)
-		    end
-		end
-	end
-end
+     describe StringCalculator do
+      subject(:kata) {described_class.new}
+       it "should expect a string" do
+       	msg="errrrrorrr!!!"
+       	expect{kata.add("2,3,5")}.to_not raise_error, "errrrrorrr"
+       end
+     end
