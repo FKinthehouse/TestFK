7ed949a2cf848e2d275303a44b1e3cab07fa0328
Tariq Khan <tariqkhan@Tariqs-MacBook-Air.local>
2024-04-30 17:51:43 +0530

    next test case

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,8 +3,18 @@ require 'StringCalculator'
 
      describe StringCalculator do
       subject(:kata) {described_class.new}
-       it "should expect a string" do
+     it "should expect a string" do
        	msg="errrrrorrr!!!"
        	expect{kata.add("2,3,5")}.to_not raise_error, "errrrrorrr"
-       end
+     end
+
+     it "should raise an error for other data types than string" do
+
+     	expect {kata.add(1,2,3)}.to raise_error, "errorr 1"
+     	expect {kata.add([55,56])}.to raise_error(RuntimeError)
+     end
+     
+ 
+
+ 
      end
